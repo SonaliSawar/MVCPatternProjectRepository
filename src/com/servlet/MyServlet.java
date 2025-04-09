f96adcc5a18161d55ff78c471039cc2a37b73f4f
HARSHAD <HARSHAD@LAPTOP-6QMVGV7T>
2025-04-09 22:26:49 +0530

    commit 2

--- src/com/servlet/MyServlet.java
+++ src/com/servlet/MyServlet.java
@@ -7,10 +7,14 @@ import javax.servlet.http.HttpServletResponse;
 public class MyServlet extends HttpServlet{
 
 	
+	
+	
 	public void doPost(HttpServletRequest req,  HttpServletResponse res)
 	{
 		int i = Integer.parseInt(req.getParameter("num1"));
 		int j = Integer.parseInt(req.getParameter("num2"));
+		System.out.print(i+j);
+		System.out.print(i+j);
 		System.out.print(i+j);
 	}
 	
